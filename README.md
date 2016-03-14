0ae6bf8c36c4a1534a96c3aac25fc51ec3cf4453
thradec <thradec@gmail.com>
2016-03-14 10:56:58 +0100

    PoC cache api

--- README.md
+++ README.md
@@ -1,25 +1,8 @@
-# FeedHenry Hello World MBaaS Server
+# testing-cloud-app
 
-This is a blank 'hello world' FeedHenry MBaaS. Use it as a starting point for building your APIs. 
-
-# Group Hello World API
-
-# hello [/hello]
-
-'Hello world' endpoint.
-
-## hello [POST] 
-
-'Hello world' endpoint.
-
-+ Request (application/json)
-    + Body
-            {
-              "hello": "world"
-            }
-
-+ Response 200 (application/json)
-    + Body
-            {
-              "msg": "Hello world"
-            }
+TODO
+- napsat readme
+- napsat index.html
+- vycistit package.json
+- vycistit grunt.js
+- validace parametru
--- application.js
+++ application.js
@@ -1,30 +1,25 @@
-var mbaasApi = require('fh-mbaas-api');
 var express = require('express');
-var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var mbaasApi = require('fh-mbaas-api');
+var mbaasExpress = mbaasApi.mbaasExpress();
 
-// list the endpoints which you want to make securable here
-var securableEndpoints;
-securableEndpoints = ['/hello'];
+var securableEndpoints = [
+  '/cache'
+];
 
 var app = express();
 
-// Enable CORS for all requests
-app.use(cors());
-
-// Note: the order which we add middleware to Express here is important!
 app.use('/sys', mbaasExpress.sys(securableEndpoints));
 app.use('/mbaas', mbaasExpress.mbaas);
-
-// allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
-
-// Note: important that this is added just before your own Routes
 app.use(mbaasExpress.fhmiddleware());
 
-app.use('/hello', require('./lib/hello.js')());
+app.use(cors());
+app.use(bodyParser());
 
-// Important that this is last!
+app.use('/cache', require('./lib/cache.js').router());
+
 app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
