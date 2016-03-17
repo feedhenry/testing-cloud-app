58922ffa1fdd55202dac6b2681ce4e74be54b2ac
thradec <thradec@gmail.com>
2016-03-17 09:20:16 +0100

    code polishing

--- application.js
+++ application.js
@@ -28,5 +28,5 @@ app.use(mbaasExpress.errorHandler());
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(port, host, function() {
-  console.log("App started at: " + new Date() + " on port: " + port); 
+  console.log('App started at: ' + new Date() + ' on port: ' + port); 
 });
